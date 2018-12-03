6efb21c3dba95a6f1a86973b817509821f0aaaf6
Douglas Franklin <>
2018-12-02 22:32:05 -0200

    install and configure firebase and create page signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
 
 import { HomePage } from '../pages/home/home';
+
+const config = {
+  apiKey: "AIzaSyAyJQhLLwEfd2gmH3wtd5MVViiQo8FZkI8",
+  authDomain: "ionic-with-firebase-96285.firebaseapp.com",
+  databaseURL: "https://ionic-with-firebase-96285.firebaseio.com",
+  projectId: "ionic-with-firebase-96285",
+  storageBucket: "ionic-with-firebase-96285.appspot.com",
+  messagingSenderId: "281913452177"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,6 +28,7 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
 
